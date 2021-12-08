5e9aceab1a507007d2163cdca80fca80376a7a60
evil3sc <hyp3rbone@outlook.com>
2021-12-08 03:21:47 -0600

    basic pair price message

--- tel-bot.py
+++ tel-bot.py
@@ -4,6 +4,9 @@ from keys import api_key, api_secret, api_key_bot
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
 import telegram.ext
+import re
+
+client = Client(api_key, api_secret)
 
 def start(update, context):
     update.message.reply_text('Welcome to crypto bot')
@@ -15,14 +18,34 @@ def help(update, context):
     /price [coin] -> Price
     ''')
 
+def price_handled(update, context):
+    if re.match(r'price ', update.message.text, re.IGNORECASE):
+        try:
+            found = re.search(r'^price (.+)', update.message.text, re.IGNORECASE).group(1)
+            update.message.reply_text(f'Precio: {found.split()[0]}')
+        except AttributeError:
+            update.message.reply_text('Comando erróneo :(')
+
 def price(update, context):
-    update.message.reply_text(f'Mensaje: {update.message.text}')
+    if len(context.args) > 0:
+        ticker = context.args[0]
+        ticker = ticker.upper()
+        if ticker.isalpha():
+            try:
+                data = client.get_margin_price_index(symbol='BTCUSDT')
+                pri = float(data['price'])
+                update.message.reply_text(f'El precio de {ticker} es {pri:.2f}')
+            except BinanceAPIException:
+                update.message.reply_text('Error de par :(')
+
 
 updater = telegram.ext.Updater(api_key_bot, use_context=True)
 disp = updater.dispatcher
 
 disp.add_handler(telegram.ext.CommandHandler('start', start))
 disp.add_handler(telegram.ext.CommandHandler('help', help))
+disp.add_handler(telegram.ext.CommandHandler('price', price))
+#disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, price_handled))
 
 updater.start_polling()
 updater.idle()
