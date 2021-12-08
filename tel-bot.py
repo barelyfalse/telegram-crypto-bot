ce1ca1c340fee1baac3c038de18b2a5a3e3c277d
evil3sc <hyp3rbone@outlook.com>
2021-12-08 01:43:51 -0600

    basic commands

--- tel-bot.py
+++ tel-bot.py
@@ -1,2 +1,28 @@
+#keys
 from keys import api_key, api_secret, api_key_bot
 
+from binance.client import Client
+from binance.exceptions import BinanceAPIException
+import telegram.ext
+
+def start(update, context):
+    update.message.reply_text('Welcome to crypto bot')
+
+def help(update, context):
+    update.message.reply_text('''
+    Comandos disponibles:
+    /start -> Welcome message
+    /price [coin] -> Price
+    ''')
+
+def price(update, context):
+    update.message.reply_text(f'Mensaje: {update.message.text}')
+
+updater = telegram.ext.Updater(api_key_bot, use_context=True)
+disp = updater.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler('start', start))
+disp.add_handler(telegram.ext.CommandHandler('help', help))
+
+updater.start_polling()
+updater.idle()
