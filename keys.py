b808451c7e32b9fb8945345f95b9c9f0066907ca
evil3sc <hyp3rbone@outlook.com>
2021-12-08 01:17:58 -0600

    keys file

---
+++ keys.py
@@ -0,0 +1,7 @@
+import os
+
+# hidden
+api_key = os.environ['api_key']
+api_secret = os.environ['api_secret']
+
+api_key_bot = os.environ['api_key_bot']
---
+++ tel-bot.py
@@ -0,0 +1,2 @@
+from keys import api_key, api_secret, api_key_bot
+
